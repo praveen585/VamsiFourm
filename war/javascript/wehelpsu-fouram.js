c18ae44a1f3812933daef9d977bc52c4db5fe3f2
Teleror1 <teleror1@teleror1-desktop.(none)>
2012-11-27 20:18:38 +0530

    first commin on jquery

--- war/javascript/wehelpsu-fouram.js
+++ war/javascript/wehelpsu-fouram.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-alert ("hai")	
+$("#comment_box_button").click(function() {
+	
+	var ctx = $('#displayer')[0].getContext("2d");
+	ctx.fillText($("#comment_box").val(), 0, 20);
+	alert(ctx.getText())
+}
+);
 });
